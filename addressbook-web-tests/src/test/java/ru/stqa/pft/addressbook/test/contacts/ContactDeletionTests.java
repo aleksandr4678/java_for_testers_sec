5e46f26a2cab339adc150822759edaa7853e01fb
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2019-11-02 02:22:07 +0300

    post-definition developed for contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactDeletionTests.java
@@ -1,11 +1,17 @@
 package ru.stqa.pft.addressbook.test.contacts;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
 public class ContactDeletionTests extends TestBase {
     @Test
     public void testContactCreation() throws Exception {
+        if (! app.getContactHelper().isThereAContact()){
+            app.getContactHelper().createContact(new ContactData("Temp", null,
+                    "Temp2", null, null,
+                    null, "temp@adg.com", "test1-1"), true);
+        }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
         app.getContactHelper().confirmContactDeleteAlert();
