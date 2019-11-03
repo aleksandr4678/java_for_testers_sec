395702563b1a63c0740600dff4029a0526579d54
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2019-11-04 01:35:56 +0300

    Tests for Contacts modifyed: situation with no one group avoided.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactDeletionTests.java
@@ -2,19 +2,29 @@ package ru.stqa.pft.addressbook.test.contacts;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
 public class ContactDeletionTests extends TestBase {
     @Test
     public void testContactCreation() throws Exception {
         if (! app.getContactHelper().isThereAContact()){
+            //new group creation, it would avoid situation when no one groups doesn't exit.
+            app.getNavigationHelper().goToGroupPage();
+            app.getGroupHelper().createGroup(new GroupData("temp_group", null, null));
+            //new contact creation
             app.getContactHelper().createContact(new ContactData("Temp", null,
                     "Temp2", null, null,
-                    null, "temp@adg.com", "test1-1"), true);
+                    null, "temp@adg.com", "temp_group"), true);
         }
+        //main test, contact deletion
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
         app.getContactHelper().confirmContactDeleteAlert();
+        //temp group deletion
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroup();
         app.getNavigationHelper().gotoHomePage();
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/contacts/ContactModificationTests.java
@@ -2,21 +2,30 @@ package ru.stqa.pft.addressbook.test.contacts;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
 public class ContactModificationTests extends TestBase {
     @Test
     public void groupModification() {
         if (! app.getContactHelper().isThereAContact()){
+            //new group creation, it would avoid situation when no one groups doesn't exit.
+            app.getNavigationHelper().goToGroupPage();
+            app.getGroupHelper().createGroup(new GroupData("temp_group", null, null));
+            //new contact creation
             app.getContactHelper().createContact(new ContactData("Temp", null,
                     "Temp2", null, null,
-                    null, "temp@adg.com", "test1-1"), true);
+                    null, "temp@adg.com", "temp_group"), true);
         }
         app.getContactHelper().editContact();
         app.getContactHelper().fillContactForm(new ContactData("ContNameEdit", "ContMiddleEdit",
                 "ContLastEdit", "CompanyOfContactEdit", "111232, tuda-to, syuda-toEdit",
-                "+74895238845", "contEdit@adg.com", "test1-1"), false);
+                "+74895238845", "contEdit@adg.com", "temp_group"), false);
         app.getContactHelper().submitContactUpdate(); //page has a bug. its delete the updated contact.
+        //temp group deletion
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroup();
         app.getNavigationHelper().gotoHomePage();
     }
 }
