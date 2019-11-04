221ed684527cc26697712ce80e374ea36f40ae8a
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2019-11-05 01:13:25 +0300

    Prime classe added (prostoe chislo) with appropriate tests. Reformat code for all sandbox executed.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,19 +2,19 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main (String [] args)
-	{
-		hello ("world");
-        hello ("user");
-        hello ("Alex");
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Alex");
 
-	    Square s = new Square(5);
-		System.out.println("Area for square with side " + s.l + " = " + s.area());
+        Square s = new Square(5);
+        System.out.println("Area for square with side " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Area for rectangle with side " + r.a + " and " + r.b + " = " + r.area());
-	}
-    public static void hello (String somebody) {
+    }
+
+    public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 }
