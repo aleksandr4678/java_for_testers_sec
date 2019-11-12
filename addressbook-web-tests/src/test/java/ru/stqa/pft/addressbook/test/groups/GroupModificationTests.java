455079232839afa9ee0a4cd354818e2da2bba296
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2019-11-13 00:28:36 +0300

    Refactor to use List<> to check that object are really equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupModificationTests.java
@@ -5,20 +5,23 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
     @Test
     public void groupModification() {
         app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
+        if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1-1", "test1-2-1", null));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
+        Assert.assertEquals(before, after);
     }
 }
