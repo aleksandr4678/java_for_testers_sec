a25eef631bab24dd6aba0cefb427ec12dd214005
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2019-11-23 01:10:19 +0300

    Sorting and lamdas

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,6 +27,11 @@ public class GroupModificationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size());
         before.remove(before.size()-1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        //List sorting
+        Comparator<? super GroupData> byId = (g1,g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+        //Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
