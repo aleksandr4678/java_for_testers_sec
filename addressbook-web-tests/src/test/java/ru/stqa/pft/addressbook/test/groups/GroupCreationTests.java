9641cd06cf8c7ef8c3a022b54aa9f5a44517d3dd
Aleksandr Kastykin <spb-alexander-78@yandex.ru>
2020-02-16 21:16:47 +0300

    Hamcrest asserts added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/groups/GroupCreationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.test.groups;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
@@ -16,9 +18,9 @@ public class GroupCreationTests extends TestBase {
         GroupData group = new GroupData().withName("test1-1");
         app.group().create(group);
         Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        assertThat(after.size(), equalTo(before.size() + 1));
         group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         before.add(group);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before));
     }
 }
